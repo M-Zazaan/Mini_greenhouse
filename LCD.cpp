05d7f9c7025708aaaee7efeae081fe4a5759c4b7
Muhammad Zazaan Qazi <zazaan12ok@gmail.com>
2026-02-14 17:38:31 +0500

    Update LCD.cpp

--- LCD.cpp
+++ LCD.cpp
@@ -1,12 +1,18 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27,16,2);
 
-void setup() {
+void setup()
+{
   lcd.init();
   lcd.backlight();
-  lcd.print("Hello Rizu!");
+
+  lcd.setCursor(0,0);
+  lcd.print("HELLO");
+
+  lcd.setCursor(0,1);
+  lcd.print("WORKING");
 }
 
 void loop() {}
