af071dec04137b581f4b970971190c808df0c78c
Muhammad Zazaan Qazi <zazaan12ok@gmail.com>
2026-02-20 20:26:34 +0500

    Update soil.cpp

--- soil.cpp
+++ soil.cpp
@@ -5,7 +5,32 @@ void setup() {
 }
 
 void loop() {
-  int value = analogRead(sensorPin);
-  Serial.println(value);
+
+  int total = 0;
+
+  for(int i=0; i<10; i++){
+    total += analogRead(sensorPin);
+    delay(10);
+  }
+
+  int moisture = total / 10;
+
+  Serial.print("Value: ");
+  Serial.print(moisture);
+  Serial.print("  ->  ");
+
+  if(moisture > 350)
+  {
+    Serial.println("LOW Moisture (Dry)");
+  }
+  else if(moisture > 280)
+  {
+    Serial.println("MEDIUM Moisture");
+  }
+  else
+  {
+    Serial.println("HIGH Moisture (Wet)");
+  }
+
   delay(1000);
 }
