4f09a59ed1d4cebcd9ddef16615056c027e67b8c
Muhammad Zazaan Qazi <zazaan12ok@gmail.com>
2026-02-25 19:48:50 +0500

    Update full_test.cpp

--- full_test.cpp
+++ full_test.cpp
@@ -1,10 +1,26 @@
+#define soilPin 7
+#define relayPin 8
+
 void setup() {
-  pinMode(8, OUTPUT);
+  pinMode(soilPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+
+  digitalWrite(relayPin, HIGH); // Pump OFF initially
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(8, LOW); // ON
-  delay(5000);
-  digitalWrite(8, HIGH); // OFF
-  delay(5000);
+
+  int soilState = digitalRead(soilPin);
+
+  Serial.println(soilState);
+
+  if (soilState == LOW) {   // If DRY
+    digitalWrite(relayPin, LOW);  // Pump ON
+  }
+  else {                    // If WET
+    digitalWrite(relayPin, HIGH); // Pump OFF
+  }
+
+  delay(500);
 }
